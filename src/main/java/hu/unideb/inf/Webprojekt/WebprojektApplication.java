1bca084df5967a541714a57df676886882aa3b61
DZoltan <55488265+DZoltan@users.noreply.github.com>
2021-12-12 13:58:42 +0100

    deleted comments

--- src/main/java/hu/unideb/inf/Webprojekt/WebprojektApplication.java
+++ src/main/java/hu/unideb/inf/Webprojekt/WebprojektApplication.java
@@ -7,10 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebprojektApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebprojektApplication.class, args);
 	}
-
 
 }
